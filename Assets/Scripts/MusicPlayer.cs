143bf0a34411fe1e7f678ba336ccc2cf38b61104
Zarapps Games <zarappsgames@gmail.com>
2020-02-01 21:33:54 +0100

    musica de fondo y click boton de inicio

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public static MusicPlayer Instance;
+    void Awake() {
+        if(Instance)
+            DestroyImmediate(gameObject);
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+    }	
+}
