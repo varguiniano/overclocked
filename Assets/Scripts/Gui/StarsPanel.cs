fa4f814dc47b2bb1f0b5dd03deb716485305117b
Zarapps Games <zarappsgames@gmail.com>
2020-02-01 21:19:04 +0100

    stars panel

---
+++ Assets/Scripts/Gui/StarsPanel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StarsPanel : MonoBehaviour
+{
+    public Image[] stars;
+
+    private int _starsNumber;
+    // Update is called once per frame
+    void Update()
+    {
+        _starsNumber = GameManager.Instance.Stars;
+        if (_starsNumber >= 10)
+        {
+            _starsNumber = 10;
+        }
+        if (_starsNumber <= 0)
+        {
+            _starsNumber = 0;
+        }
+        
+        _starsNumber = _starsNumber / 2;
+        for (int i = 0; i < stars.Length; i++)
+        {
+            stars[i].gameObject.SetActive(false);
+        }
+        for (int i = 0; i < _starsNumber; i++)
+        {
+            stars[i].gameObject.SetActive(true);
+        }
+    }
+}
