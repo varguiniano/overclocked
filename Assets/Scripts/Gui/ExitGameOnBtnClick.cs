56a4680adfe83509ba468128f6df85273290715a
Rodrigo Varga <varguiniano@gmail.com>
2020-02-02 11:08:11 +0100

    - New Game Over animation.

---
+++ Assets/Scripts/Gui/ExitGameOnBtnClick.cs
@@ -0,0 +1,13 @@
+﻿using Varguiniano.Core.Runtime.Common;
+using Varguiniano.Core.Runtime.Gui;
+
+namespace Gui
+{
+    public class ExitGameOnBtnClick : ActionOnButtonClick
+    {
+        protected override void ButtonClicked()
+        {
+            Utils.CloseGame();
+        }
+    }
+}
